3a11acb92318059ac97e8a7104015571aaeddbb5
dyushkin93 <d.yushkin93@gmail.com>
2020-07-11 19:51:17 +0500

    feat: complete inputing, language change and some navigation

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const ruUpperCases = ["Ё", "!", "\"", "№", ";", "%", ":", "?", "*", "(", ")",
 
 const enUpperCases = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Backspace", "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "Enter", "Shift", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "Shift", "Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight"];
 
-const enLowerCases = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Shift", "Ctrl", "Win", "Alt", "space", "Alt", "Ctrl", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight"];
+const enLowerCases = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Shift", "Ctrl", "Win", "Alt", "space", "Alt", "Ctrl", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight"]; 
+
+let language = "EN";
 
 class Key {
   constructor(keyCode, enLowerCase, enUpperCase, ruLowerCase, ruUpperCase) {
@@ -15,11 +17,12 @@ class Key {
     this.enUpperCase = enUpperCase;
     this.ruLowerCase = ruLowerCase;
     this.ruUpperCase = ruUpperCase;
+    this.init();
+    this.pressed = false;
   }
-
   init() {
     this.block = document.createElement("div");
-    if (/^(Backspace|Tab|CapsLock|Enter|Shift|Control|Alt|Meta|Space)/.test(this.keyCode)) { // set names of functional keys
+    if (/^(Backspace|Tab|CapsLock|Enter|Shift|Control|Alt|Meta|Space)/.test(this.keyCode)) { // set titles of modifier keys
       this.block.className = `modifier-key ${this.enLowerCase.toLowerCase()}`;
       this.block.innerHTML = `<p>${this.enLowerCase}</p>`;
     } else if (/^Arrow/.test(this.keyCode)) { // set arrow keys icon
@@ -44,7 +47,7 @@ class Key {
         default: 
           break;
       }
-    } else if (/^Key/.test(this.keyCode)) { // set letter keys name
+    } else if (/^Key/.test(this.keyCode)) { // set letter keys title
       this.block.className = "letter-key small-key";
       this.block.innerHTML = `<div class="title english">${this.enUpperCase}</div><div class="title russian">${this.ruUpperCase}</div>`
     } else {
@@ -73,12 +76,173 @@ keyboardFrame.className = "keyboard-wrapper";
 keyboard.className = "keyboard";
 
 keyboardFrame.append(textArea, keyboard);
-keyboardFrame.insertAdjacentHTML("beforeend", "<p>Change language (RU / ENG): Alt + Shift</p>")
+keyboardFrame.insertAdjacentHTML("beforeend", "<p>Change language (RU / ENG): Alt + Shift</p><p>&#9888 Language may be different from current input language of your PC</p>")
 document.body.append(keyboardFrame);
 
-let keys = [];
+
+let Keyboard = {};
+
 for (let i = 0; i < keyCodes.length; i++) {
-  keys.push(new Key(keyCodes[i], enLowerCases[i], enUpperCases[i], ruLowerCases[i], ruUpperCases[i]));
-  keys[i].init();
-  keyboard.append(keys[i].block);
+  Keyboard[keyCodes[i]] = new Key(keyCodes[i], enLowerCases[i], enUpperCases[i], ruLowerCases[i], ruUpperCases[i]);
+}
+
+for (let key in Keyboard) {
+  keyboard.append(Keyboard[key].block);
+}
+
+let keyActions = {
+  input(letter) {
+    textArea.setRangeText(letter, textArea.selectionStart, textArea.selectionEnd, "end");
+  },
+  delete() {
+    if (textArea.selectionStart != 0) {
+      if (textArea.selectionStart === textArea.selectionEnd) {
+        textArea.setRangeText("", textArea.selectionStart - 1, textArea.selectionEnd, "end");
+      } else {
+        textArea.setRangeText("", textArea.selectionStart, textArea.selectionEnd, "end");
+      }
+    }
+  },
+  unpressAll() {
+    for (let key in Keyboard) {
+      Keyboard[key].pressed = false;
+      Keyboard[key].block.classList.remove("pressed");
+    }
+  },
+  modifierToggle(keyObj) {
+    return keyObj.pressed === true ? keyObj.pressed = false : keyObj.pressed = true;
+  },
+  changeLang () {
+    language === "EN" ? language = "RU" : language = "EN";
+    ruTitles = document.querySelectorAll(".russian");
+    enTitles = document.querySelectorAll(".english");
+    if (language === "EN") {
+      ruTitles.forEach(element => {
+        element.style.display = "none";
+      });
+      enTitles.forEach(element => {
+        element.style.display = "block";
+      });
+    } else {
+      ruTitles.forEach(element => {
+        element.style.display = "block";
+      });
+      enTitles.forEach(element => {
+        element.style.display = "none";
+      });
+    }
+  }
+}
+
+// add handler for mouse click
+
+for (let key in Keyboard) {
+  Keyboard[key].block.addEventListener("mousedown", e => {
+    e.preventDefault();
+    textArea.focus();
+    if (/^(CapsLock)/.test(Keyboard[key].keyCode)) {
+      Keyboard[key].block.classList.add("pressed");
+      keyActions.modifierToggle(Keyboard[key]);
+    } else if (/^(Shift)/.test(Keyboard[key].keyCode)) {
+      if (Keyboard.AltLeft.pressed === true || Keyboard.AltRight.pressed === true) {
+        keyActions.changeLang();
+        Keyboard[key].block.classList.add("pressed");
+      } else {
+        Keyboard[key].block.classList.toggle("pressed");
+        keyActions.modifierToggle(Keyboard[key]);
+      }
+    } else if (/^(Alt)/.test(Keyboard[key].keyCode)) {
+      if (Keyboard.ShiftLeft.pressed === true || Keyboard.ShiftRight.pressed === true) {
+        keyActions.changeLang();
+        Keyboard[key].block.classList.add("pressed");
+      } else {
+        Keyboard[key].block.classList.toggle("pressed");
+        keyActions.modifierToggle(Keyboard[key]);
+      }
+    } else if (/^Key/.test(Keyboard[key].keyCode)) {
+      Keyboard[key].block.classList.add("pressed");
+      if ((Keyboard.CapsLock.pressed === true || Keyboard.ShiftRight.pressed === true || Keyboard.ShiftLeft.pressed === true) && language === "EN") {
+        keyActions.input(Keyboard[key].enUpperCase);
+      } else if ((Keyboard.CapsLock.pressed === true || Keyboard.ShiftRight.pressed === true || Keyboard.ShiftLeft.pressed === true) && language === "RU") {
+        keyActions.input(Keyboard[key].ruUpperCase);
+      } else if (language === "EN") {
+        keyActions.input(Keyboard[key].enLowerCase);
+      } else {
+        keyActions.input(Keyboard[key].ruLowerCase);
+      }
+    } else if (/^Arrow/.test(Keyboard[key].keyCode)) {
+      //move target
+      switch (Keyboard[key].keyCode) {
+        case "ArrowLeft":
+          if (textArea.selectionStart != 0) {
+            textArea.selectionStart = textArea.selectionStart - 1;
+            textArea.selectionEnd = textArea.selectionStart;
+          }
+          break;
+        case "ArrowUp":
+          
+          break;
+        case "ArrowDown":
+          
+          break;
+        case "ArrowRight":
+          textArea.selectionStart = textArea.selectionStart + 1;
+          textArea.selectionEnd = textArea.selectionStart;
+          break;
+        default: 
+          break;
+      }
+    } else if (/Backspace/.test(Keyboard[key].keyCode)) {
+      //delete char or selected
+      keyActions.delete();
+    } else if (/Enter/.test(Keyboard[key].keyCode)) {
+      keyActions.input("\n");
+    } else if (/Tab/.test(Keyboard[key].keyCode)) {
+      keyActions.input("  ");
+    } else if (/BracketLeft|BracketRight|Semicolon|Quote|Comma|Period/.test(Keyboard[key].keyCode)) { //keys with only russian letters
+      Keyboard[key].block.classList.add("pressed");
+      if ((Keyboard.ShiftRight.pressed === true || Keyboard.ShiftLeft.pressed === true) && language === "EN") {
+        keyActions.input(Keyboard[key].enUpperCase);
+      } else if ((Keyboard.CapsLock.pressed === true || Keyboard.ShiftRight.pressed === true || Keyboard.ShiftLeft.pressed === true) && language === "RU") {
+        keyActions.input(Keyboard[key].ruUpperCase);
+      } else if (language === "EN") {
+        keyActions.input(Keyboard[key].enLowerCase);
+      } else {
+        keyActions.input(Keyboard[key].ruLowerCase);
+      }
+    } else if (!/Meta/.test(Keyboard[key].keyCode)) { // all other keys
+      Keyboard[key].block.classList.add("pressed");
+      if ((Keyboard.ShiftRight.pressed === true || Keyboard.ShiftLeft.pressed === true) && language === "EN") {
+        keyActions.input(Keyboard[key].enUpperCase);
+      } else if ((Keyboard.ShiftRight.pressed === true || Keyboard.ShiftLeft.pressed === true) && language === "RU") {
+        keyActions.input(Keyboard[key].ruUpperCase);
+      } else if (language === "EN") {
+        keyActions.input(Keyboard[key].enLowerCase);
+      } else {
+        keyActions.input(Keyboard[key].ruLowerCase);
+      }
+    }
+  })
+}
+
+for (let key in Keyboard) {
+  Keyboard[key].block.addEventListener("mouseup", e => {
+    if (Keyboard[key].enLowerCase === "Shift" && (Keyboard.AltLeft.pressed === true || Keyboard.AltRight.pressed === true)) {
+      Keyboard.AltLeft.block.classList.remove("pressed");
+      Keyboard.AltRight.block.classList.remove("pressed");
+      Keyboard[key].block.classList.remove("pressed");
+      Keyboard.AltLeft.pressed = false;
+      Keyboard.AltRight.pressed = false;
+    } else if (Keyboard[key].enLowerCase === "Alt" && (Keyboard.ShiftLeft.pressed === true || Keyboard.ShiftRight.pressed === true)) {
+      Keyboard.ShiftLeft.block.classList.remove("pressed");
+      Keyboard.ShiftRight.block.classList.remove("pressed");
+      Keyboard[key].block.classList.remove("pressed");
+      Keyboard.ShiftLeft.pressed = false;
+      Keyboard.ShiftRight.pressed = false;
+    } else if (/CapsLock/.test(Keyboard[key].keyCode) && Keyboard[key].pressed === false) {
+      Keyboard[key].block.classList.remove("pressed");
+    } else if ((!/^(Shift|Alt|Caps)/.test(Keyboard[key].enLowerCase))) {
+      Keyboard[key].block.classList.remove("pressed");
+    }
+  })
 }
